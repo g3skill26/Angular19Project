01a7876eba6d7524ec686b991e6ea87aea8230f5
kkrish <kamakshiuwgt01@gmail.com>
2026-06-24 23:19:23 -0400

    Removed parent and child component for better naming convention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ParentComponentComponent } from "./parent-component/parent-component.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ParentComponentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'TestParentChildRoutingProject';
 }
