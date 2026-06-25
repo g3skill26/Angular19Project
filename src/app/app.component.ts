3619d6cbb1b6435eeb9f862fd7fee65b10c44100
kkrish <kamakshiuwgt01@gmail.com>
2026-06-24 23:47:58 -0400

    Renamed child and parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from "./parent/parent.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, ParentComponent],
+ templateUrl: './app.component.html',
+  //For multilines - use backtick , you can embed template inline or in seperate file 
+  //  like templateURL
+  // template:`<h1>this is html from ts file</h1>
+  // <p>para</p>`,
   styleUrl: './app.component.css'
+ // standalone: true
+  //You can styles like this 
+  // styles:[
+  //   `h1{ 
+  //     padding :10px,
+  //     background-color:blue
+  //     },
+  //     h2{
+  //       padding:10px
+  //     }`
+  //]
 })
 
 export class AppComponent {
