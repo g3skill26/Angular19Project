664f022012777a897300165fdb958fcff9966661
kkrish <kamakshiuwgt01@gmail.com>
2026-06-24 20:52:51 -0400

    Initial commit

---
+++ src/app/parent-component/parent-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-component',
+  imports: [],
+  templateUrl: './parent-component.component.html',
+  styleUrl: './parent-component.component.css'
+})
+export class ParentComponentComponent {
+
+}
